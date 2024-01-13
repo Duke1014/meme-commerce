f2b00b7f6b0974d4419163e37f8d1ebce3fea9a1
Duke Norsworthy <duke.norsworthy@gmail.com>
2024-01-13 00:44:01 -0500

    app and models work

--- app.py
+++ app.py
@@ -1,11 +1,58 @@
-from flask import Flask
+import os
+
+from flask import Flask, make_response, jsonify, request, session
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from flask_bcrypt import Bcrypt
+
+from models import db, User
 
 app = Flask(__name__)
+# app.secret_key = ''
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-@app.route("/users")
-def users():
-    pass
+migrate = Migrate(app, db)
+db.init_app(app)
 
+api = Api(app)
+
+bcrypt = Bcrypt(app)
+
+class CheckSession(Resource):
+
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else: 
+            return {}, 401
+        
+class ClearSession(Resource):
+
+    def delete(self):
+        
+        session['user_id'] = None
+
+        return {}, 204
+
+class Login(Resource):
+
+    def post(self):
+        username = request.get_json().get('username')
+        user = User.query.filter(User.username == username).first()
+        password = request.get_json().get('password')
+
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        
+        return {}, 401
+
+
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(ClearSession, '/clear_session')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
--- models.py
+++ models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
+
+from app import bcrypt, hashlib
 
 metadata = MetaData(naming_convention={
     'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
@@ -8,9 +11,36 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+def hash_email(email):
+    hashed_email = hashlib.sha256(email.encode('utf-8')).hexdigest()
+    return hashed_email
+
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True)
     email = db.Column(db.String, unique=True)
+    _password_hash = db.Column(db.String, nullable=False)
+    hashed_email = db.Column(db.String, nullable=False)
+
+    def set_email(self, email):
+        self.hashed_email = hash_email(email)
+
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate_password(self,password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+    def authenticate_email(self, email):
+        hashed_email = hash_email(email)
+        return self.hashed_email == hashed_email
